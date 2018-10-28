28a88fc84fa306bba81285ac3f7b6cc6b401a0b4
Innermadness <380361138@qq.com>
2018-10-28 15:25:30 +0800

    clear test stuff

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import axios from 'axios';
-import styles from './App.less';
-import Comp1 from './components/Comp1';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      requestResult: ''
+      text: 'hello, world!'
     };
-    console.log(process.env.NODE_ENV);
-  }
-
-  componentDidMount() {
-    console.log('AppDidMount');
-    axios.get('/mock/test.json').then(res => {
-      console.log(res.data);
-      if (res.data.code === 200) {
-        this.setState({ requestResult: res.data.result.content });
-      }
-    }).catch(err => {
-      console.log(err.message);
-    });
   }
 
   render() {
-    const { requestResult } = this.state;
+    const { text } = this.state;
     return (
-      <div className={styles.camelTitle}>
-        Hello, world!
-        <Comp1 />
-        <p>{requestResult}</p>
-      </div>
+      <div>{text}</div>
     );
   }
 }
 
-export default hot(module)(App); // 对于react，HMR要使用特殊的包
+export default hot(module)(App);
